463b2190cb75f0186c4e3c52846995a11f886efa
gupta <gupta@LAPTOP-03JQG6Q6>
2021-03-31 15:00:26 +0530

    Duplicate characters in String in Java

---
+++ src/com/java/DuplicateCharacters.java
@@ -0,0 +1,39 @@
+package com.java;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(s);
+		createMap(s);
+	}
+	static void createMap(String s)
+	{
+		Map<Character,Integer> find=new HashMap<>();
+		for(int i=0;i<s.length();i++)
+		{
+			if(find.containsKey(s.charAt(i)))
+			{
+				find.put( s.charAt(i) , find.get(s.charAt(i))+1);
+			}
+			else
+			{
+				find.put(s.charAt(i),1);
+			}
+		}
+		System.out.println(find);
+		for (Map.Entry<Character,Integer> entry:find.entrySet())
+		{
+			if(entry.getValue()>1)
+			{
+				System.out.println(entry.getKey() + " value " + entry.getValue());
+			}
+		}
+		
+	}
+	
+}
